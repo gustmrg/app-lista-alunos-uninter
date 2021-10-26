2cc94bde743d8dad354d04f6a9a9b5d10409956d
Gustavo Miranda <gmiranda90@outlook.com>
2021-10-25 23:03:27 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Aluno } from 'src/models/aluno'; // importa o modelo criado de aluno e suas propriedades
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AppAlunos';
+
+  public alunos: Aluno[] = []; // cria um array do tipo Alunos conforme o modelo importado
+  public title: String = 'Lista de Alunos'; // variável que guarda o título da página que será renderizado no html
+
+  // iniciamos o construtor e acrescentamos todos os alunos com o modelo Aluno importado de aluno.ts 
+  // passando os valores das propriedades diretamente na criação e seguindo a ordem da classe
+  // conforme o método construtor da classe (RU, Nome, Curso, Data de Nascimento)
+  constructor() {
+    this.alunos.push(new Aluno(3294435, "Gustavo Miranda", "Análise e Desenvolvimento de Sistemas", "09/02/1990"));
+    this.alunos.push(new Aluno(1225444, "Sandra Lima", "Bacharelado em Administração", "08/04/1996"));
+    this.alunos.push(new Aluno(8547558, "Tiago Caldeira", "Tecnologia em Design de Games ", "15/12/1989"));
+    this.alunos.push(new Aluno(8484885, "Larissa Cardoso", "Bacharelado em Jornalismo", "10/01/2001"));
+    this.alunos.push(new Aluno(1335775, "Jaqueline Cordeiro", "Psicologia Clínica", "07/03/1994"));
+  }
 }
---
+++ src/models/aluno.ts
@@ -0,0 +1,11 @@
+// Classe que possui as propriedades com os dados de cada aluno
+
+export class Aluno {
+
+    constructor(
+        public ru: Number,
+        public nome: String,
+        public curso: String,
+        public nasc: String
+    ) { }
+}
